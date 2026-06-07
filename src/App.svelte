a5690783677b0b3267a3d527275ec896b94dbef6
Guillaume Bagan <gbagan@gmail.com>
2026-06-07 12:29:32 +0200

    code cleaning

--- src/App.svelte
+++ src/App.svelte
@@ -14,18 +14,18 @@
     const parsed = input.split('\n').map(s => s.trim()).filter(s => s.length > 0);
     try {
       if (parsed.length === 0) throw Error("Entrée vide");
+      if (parsed.length > 18) throw Error("Limité à 18 personnes");
       for (const line of parsed) {
         const words = line.split(' ').map(s => s.trim()).filter(s => s.length > 0);
         if (words.length !== 2) throw Error(`Ligne "${line}" invalide`);
         const [name, label] = words;
         if (label.length > 2) throw Error("Les labels doivent avoir au plus 2 caractères")
-        if(nameSet.has(name)) throw Error(`Le prénom "${name}" apparait deux fois`);
-        if(labelSet.has(label)) throw Error(`Le label "${label}" apparait deux fois`);
-        nameSet.add(words[0]);
-        labelSet.add(words[1]);
+        if (nameSet.has(name)) throw Error(`Le prénom "${name}" apparait deux fois`);
+        if (labelSet.has(label)) throw Error(`Le label "${label}" apparait deux fois`);
+        nameSet.add(name);
+        labelSet.add(label);
         res.push({ name, label });
       }
-      if (res.length > 18) throw Error("Limité à 18 personnes");
       names = res;
       await tick();
       window.print();
